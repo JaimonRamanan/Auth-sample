f22dccb0d885b3801d2826d6f1afc5844bb75209
JaimonRamanan <dev.jaimon@gmail.com>
2024-05-11 22:10:24 +0530

    fix: import

--- lib/application/auth/auth_bloc.dart
+++ lib/application/auth/auth_bloc.dart
@@ -1,6 +1,6 @@
-import 'package:auth_sample/core/constants.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:auth_sample/core/constants.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'auth_state.dart';
